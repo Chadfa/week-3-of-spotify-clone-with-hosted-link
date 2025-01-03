bad86977ad901a26d3d07e20a366eaa31be73d59
Sheik frd <168510817+Chadfa@users.noreply.github.com>
2025-01-03 17:27:41 +0530

    Add files via upload

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from "express"
+import cors from 'cors'
+import connectCloudinary from "./src/config/cloudinary.js";
+import 'dotenv/config'
+import connectDB from "./src/config/mongodb.js";
+import songRouter from "./src/routes/songRoute.js";
+import albumRouter from "./src/routes/albumRoute.js";
+
+const app = express()
+const port = process.env.PORT || 4000
+connectCloudinary()
+connectDB()
+
+app.use(express.json())
+app.use(cors())
+
+app.use("/api/song", songRouter )
+app.use("/api/album", albumRouter )
+
+app.get("/", (req, res) => res.send("API Working"))
+
+app.listen(port, () => console.log(`Server started on ${port}`))
